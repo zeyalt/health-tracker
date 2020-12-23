e8519b9e0350d4256ea7a5b8d9f01a1f9c5d1043
Zeya <zeya.zlt@gmail.com>
2020-12-23 08:22:41 +0800

    23 Dec, 8.22am

--- app.py
+++ app.py
@@ -10,203 +10,210 @@ from pathlib import Path
 import sqlite3
 from sqlite3 import Connection
 import streamlit as st
-import datetime
+import datetime as dt
 import plotly.graph_objects as go
+import base64
+from io import BytesIO
 
-URI_SQLITE_DB = "lwinhealthrecords.db"
+DATABASE_NAME = "myint_lwin_health_records"
+URI_SQLITE_DB = DATABASE_NAME + ".db"
 
 def main():
     st.title("My Health Tracker")
-    st.markdown("Use this web application to update and visualise your health records.")    
+    st.markdown("Use this web application to store, update and monitor your health records.")
+    s1, s2, s3 = st.beta_columns([1, 1, 1])
+    s1.write("Name of Database: ")
+    s2.text(DATABASE_NAME)
     conn = get_connection(URI_SQLITE_DB)
     init_db(conn)
-    
-    health_record = st.selectbox("Select health record to update:", ["Blood Pressure", "Resting Heart Rate", "Body Mass Index"])
-    if health_record == "Blood Pressure":
+    health_record = st.selectbox("Select health record to update", ["Blood Pressure", "Resting Heart Rate", "Body Mass Index"])
 
-        input_ex = st.beta_expander("Input")
-        a1, a2, a3 = input_ex.beta_columns(3)
+    if health_record == "Blood Pressure":
+        a1, a2, a3 = st.beta_columns([1, 2, 2])
+        date = a1.date_input('Date of measurement', dt.date.today(), key='1')
+        date = date.strftime("%Y-%m-%d") 
+        lower = a2.slider("Diastolic (lower) blood pressure (mmHg)", 40, 120)
+        upper = a3.slider("Systolic (upper) blood pressure (mmHg)", 60, 180)
         
-        date = a1.date_input('Enter date of measurement:', datetime.date.today(), key='1')
-        date = date.strftime("%d %b %Y")
-        lower = a2.slider("Enter diastolic (lower) blood pressure (mmHg):", 40, 200)
-        upper = a3.slider("Enter systolic (upper) blood pressure (mmHg):", 40, 200)
-        
-        if input_ex.button("Save record", key='1'):
+        if st.button("Save record", key='1'):
             conn.execute(f"INSERT INTO blood_pressure_table (Date, Lower, Upper) VALUES (?, ?, ?)", (date, lower, upper))
             conn.commit()
-        
-        output_ex = st.beta_expander("Output")
-        
-        view_mode = output_ex.radio("Select view mode:", ["Table", "Chart"], key='1')
+
+        output_ex = st.beta_expander("View Output")
+        view_mode = output_ex.radio("Select view mode", ["Table", "Chart"], key='1')
         df_blood_pressure = get_blood_pressure_data(conn)
-        
+
         if view_mode == "Table":
-            output_ex.table(df_blood_pressure)        
+            output_ex.table(df_blood_pressure)
         else:
             fig1 = plot_blood_pressure(df_blood_pressure)
             output_ex.plotly_chart(fig1)
-            
             fig2 = plot_blood_pressure_line(df_blood_pressure)
             output_ex.plotly_chart(fig2)
-            
-    
-    elif health_record == "Resting Heart Rate":
-        input_ex = st.beta_expander("Input")
-        b1, b2 = input_ex.beta_columns(2)
-        date = b1.date_input('Enter date of measurement:', datetime.date.today(), key='2')
-        date = date.strftime("%d %b %Y")   
-        heart_rate = b2.slider("Enter resting heart rate (beats per minute):", 40, 150)
+        
+        # st.markdown(get_table_download_link(df_blood_pressure), unsafe_allow_html=True)
 
-        if input_ex.button("Save record", key='2'):
+
+    elif health_record == "Resting Heart Rate":
+        b1, b2 = st.beta_columns(2)
+        date = b1.date_input('Date of measurement', dt.date.today(), key='2')
+        date = date.strftime("%Y-%m-%d")  
+        heart_rate = b2.slider("Resting heart rate (beats per minute)", 40, 150)
+
+        if st.button("Save record", key='2'):
             conn.execute(f"INSERT INTO heart_rate_table (Date, HeartRate) VALUES (?, ?)", (date, heart_rate))
             conn.commit()
         
-        output_ex = st.beta_expander("Output")
-        view_mode = output_ex.radio("Select view mode:", ["Table", "Chart"], key='2')
+        output_ex = st.beta_expander("View Output")
+        view_mode = output_ex.radio("Select view mode", ["Table", "Chart"], key='2')
         df_heart_rate = get_heart_rate_data(conn)
         
         if view_mode == "Table":
             output_ex.table(df_heart_rate)
-            
         else:
             fig = plot_heart_rate(df_heart_rate)
             output_ex.plotly_chart(fig)
 
     elif health_record == "Body Mass Index":
-        input_ex = st.beta_expander("Input")
-        c1, c2, c3 = input_ex.beta_columns(3)
-        date = c1.date_input('Enter date of measurement:', datetime.date.today(), key='3')
-        date = date.strftime("%d %b %Y")   
-        weight = c2.slider("Enter weight (kg):", 63.0, 75.0, value=67.3, step=0.1)
+        c1, c2, c3 = st.beta_columns(3)
+        date = c1.date_input('Date of measurement', dt.date.today(), key='3')
+        date = date.strftime("%Y-%m-%d")    
+        weight = c2.slider("Weight (kg)", 63.0, 75.0, value=67.3, step=0.1)
         weight = round(weight, 1)
-        height = c3.slider("Enter height (m):", 1.6, 1.8, value=1.72, step=0.01)
+        height = c3.slider("Height (m)", 1.6, 1.8, value=1.72, step=0.01)
         height = round(height, 2)
         bmi = round(weight / (height**2), 1)
-        bmi1, bmi2, bmi3 = input_ex.beta_columns(3)
+        bmi1, bmi2, bmi3 = st.beta_columns(3)
         bmi1.write("Your BMI is ")
         bmi2.header(str(bmi))
-        
-        if input_ex.button("Save record", key='3'):
+
+        if st.button("Save record", key='3'):
             conn.execute(f"INSERT INTO bmi_table (Date, Weight, Height, BMI) VALUES (?, ?, ?, ?)", (date, weight, height, bmi))
             conn.commit()
-            
-        output_ex = st.beta_expander("Output")
-        view_mode = output_ex.radio("Select view mode:", ["Table", "Chart"], key='3')
+
+        output_ex = st.beta_expander("View Output")
+        view_mode = output_ex.radio("Select view mode", ["Table", "Chart"], key='3')
         df_bmi = get_bmi_data(conn)
         
         if view_mode == "Table":
             output_ex.table(df_bmi)
-            
         else:
-            fig1 = plot_weight(df_bmi) # STOPPED HERE
+            fig1 = plot_weight(df_bmi)
             fig2 = plot_bmi(df_bmi)
             output_ex.plotly_chart(fig1)
             output_ex.plotly_chart(fig2)
 
-@st.cache
+# def to_excel(df):
+#     output = BytesIO()
+#     writer = pd.ExcelWriter(output, engine='xlsxwriter')
+#     df.to_excel(writer, index=False, sheet_name='Sheet1')
+#     writer.save()
+#     processed_data = output.getvalue()
+#     return processed_data
+
+# def get_table_download_link(df):
+#     """Generates a link allowing the data in a given panda dataframe to be downloaded
+#     in:  dataframe
+#     out: href string
+#     """
+#     # csv = df.to_csv(index=False)
+#     csv = to_excel(df)
+#     b64 = base64.b64encode(val)
+    
+#     return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="extract.xlsx">Download csv file</a>' # decode b'abc' => abc
+#     # b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+#     # href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
+#     # href = f'<a href="data:file/csv;base64,{b64}" download="myfilename.csv">Download csv file</a>'
+    
+#     # return href
+
+@st.cache(allow_output_mutation=True)
 def plot_heart_rate(data):
-    
-    fig = go.Figure()
-    
+    data = data.sort_values('Date')
+    fig = go.Figure()    
     fig.add_trace(go.Scatter(
         x=list(data['Date']),
         y=list(data['Resting Heart Rate']), 
         connectgaps=True
     ))
-
     fig.update_yaxes(
             title_text = "Resting Heart Rate (beats per minute)")
-
     fig.update_layout(
         title="Resting Heart Rate")
 
     return fig
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def plot_weight(data):
-    
+    data = data.sort_values('Date')
     fig = go.Figure()
-    
     fig.add_trace(go.Scatter(
         x=list(data['Date']),
         y=list(data['Weight']), 
         connectgaps=True 
     ))
-
     fig.update_yaxes(
             title_text = "Weight (kg)")
-
     fig.update_layout(
         title="Weight")
     
     return fig
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def plot_bmi(data):
-    
+    data = data.sort_values('Date')
     fig = go.Figure()
-    
     fig.add_trace(go.Scatter(
         x=list(data['Date']),
         y=list(data['BMI']), 
         connectgaps=True 
     ))
-
     fig.update_yaxes(
             title_text = "BMI")
-
     fig.update_layout(
         title="Body Mass Index")
 
     return fig
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def plot_blood_pressure(data):
+    data = data.sort_values('Date')
     x = [i for i in list(data["Date"]) for j in range(3)]
     y = []
-
     for i, j in data.iterrows():
         y.append(j["Diastolic (Lower)"])
         y.append(j["Systolic (Upper)"])
         y.append(None)
- 
     fig = go.Figure()
-    
     fig.add_trace(go.Scatter(
         x=x,
         y=y,
         name='Gaps',
         connectgaps=False
     ))
-    
     fig.update_yaxes(
             title_text = "Blood Pressure (mmHg)")
-    
     fig.update_layout(
         title="Blood Pressure")
-    
+
     return fig
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def plot_blood_pressure_line(data):
-                             
+    data = data.sort_values('Date')
     fig = go.Figure()
-    
     fig.add_trace(go.Scatter(
-        x=list(data['Date']),
-        y=list(data['Diastolic (Lower)']),
+        x=data['Date'],
+        y=data['Diastolic (Lower)'],
         name='Diastolic Blood Pressure',
-        connectgaps=True # override default to connect the gaps
+        connectgaps=True 
     ))
-    
     fig.add_trace(go.Scatter(
-        x=list(data['Date']),
-        y=list(data['Systolic (Upper)']), 
+        x=data['Date'],
+        y=data['Systolic (Upper)'], 
         name='Systolic Blood Pressure',
-        connectgaps=True # override default to connect the gaps
+        connectgaps=True 
     ))
-    
     fig.update_layout(legend=dict(
         orientation="h",
         yanchor="bottom",
@@ -214,15 +221,12 @@ def plot_blood_pressure_line(data):
         xanchor="right",
         x=1
     ))
-    
     fig.update_yaxes(
             title_text = "Blood Pressure (mmHg)")
-    
     fig.update_layout(
         title="Blood Pressure (Line Chart)")
-    
-    return fig
 
+    return fig
 
 def init_db(conn: Connection):
     conn.execute(
@@ -257,15 +261,27 @@ def init_db(conn: Connection):
 def get_blood_pressure_data(conn: Connection):
     df = pd.read_sql("SELECT * FROM blood_pressure_table", con=conn)
     df.columns = ['Date', 'Diastolic (Lower)', 'Systolic (Upper)']
+    df['Date'] = pd.to_datetime(df['Date'], format="%Y-%m-%d")
+    df['Date'] = df['Date'].dt.strftime("%d-%m-%Y")
+    df['Date'] = pd.to_datetime(df['Date'], format="%d-%m-%Y")
+
     return df
 
 def get_heart_rate_data(conn: Connection):
     df = pd.read_sql("SELECT * FROM heart_rate_table", con=conn)
     df.columns = ['Date', 'Resting Heart Rate']
+    df['Date'] = pd.to_datetime(df['Date'], format="%Y-%m-%d")
+    df['Date'] = df['Date'].dt.strftime("%d-%m-%Y")
+    df['Date'] = pd.to_datetime(df['Date'], format="%d-%m-%Y")
+
     return df
 
 def get_bmi_data(conn: Connection):
     df = pd.read_sql("SELECT * FROM bmi_table", con=conn)
+    df['Date'] = pd.to_datetime(df['Date'], format="%Y-%m-%d")
+    df['Date'] = df['Date'].dt.strftime("%d-%m-%Y")
+    df['Date'] = pd.to_datetime(df['Date'], format="%d-%m-%Y")
+
     return df
 
 @st.cache(hash_funcs={Connection: id})
